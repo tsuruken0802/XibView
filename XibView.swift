1da52e88b23c856dd31495ddfe8f038544d677ce
Tsuruken <tsurumotokentarou@gmail.com>
2018-08-07 18:08:04 +0900

    Update XibView.swift

--- XibView.swift
+++ XibView.swift
@@ -23,6 +23,7 @@ class XibView: UIView {
     private func loadView() {
         let className: String = String(describing: type(of: self))
         let view: UIView = Bundle.main.loadNibNamed(className, owner: self, options: nil)?.first as! UIView
+        view.backgroundColor = .clear
         view.frame = self.bounds
         self.addSubview(view)
     }
