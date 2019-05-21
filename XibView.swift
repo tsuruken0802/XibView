43e274b615a826c0223ffdbff0b76a15a84e72da
Tsuruken <tsurumotokentarou@gmail.com>
2019-05-21 14:16:46 +0900

    コメントアウト削除

--- XibView.swift
+++ XibView.swift
@@ -1,6 +1,6 @@
 //
 //  XibView.swift
-//  TimeTreeLike
+//
 //
 //  Created by 鶴本賢太朗 on 2018/05/22.
 //  Copyright © 2018年 Kentarou. All rights reserved.
@@ -9,17 +9,14 @@
 import UIKit
 
 class XibView: UIView {
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.loadView()
     }
-    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.loadView()
     }
-    
     private func loadView() {
         let className: String = String(describing: type(of: self))
         let view: UIView = Bundle.main.loadNibNamed(className, owner: self, options: nil)?.first as! UIView
